6f8fa78a8effffb521d28cb4b432724c15cb8a81
Taeuk Gwak <gwak2837@kakao.com>
2021-07-01 14:36:12 +0900

    Clean codes

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import morgan from 'morgan'
 import cookieParser from 'cookie-parser'
 import session from 'express-session'
 import { config } from 'dotenv'
-import { join } from 'path'
 import cors from 'cors'
 
 config()
