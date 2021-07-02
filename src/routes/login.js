66d8ac63dd40b09df014a97fa12133ac9e8388dc
Taeuk Gwak <gwak2837@kakao.com>
2021-07-02 09:26:22 +0900

    Handle unexpected error

--- src/routes/login.js
+++ src/routes/login.js
@@ -7,24 +7,29 @@ const { compare } = bcrypt
 const router = Router()
 
 router.post('/', async (req, res) => {
-  const { email, password } = req.body
+  try {
+    const { email, password } = req.body
 
-  const rows = await getUserByEmail(email)
+    const rows = await getUserByEmail(email)
 
-  if (rows.length === 0) {
-    res.send({ message: '로그인에 실패했어요. 이메일 또는 비밀번호를 확인해주세요.' })
-    return
+    if (rows.length === 0) {
+      res.send({ message: '로그인에 실패했어요. 이메일 또는 비밀번호를 확인해주세요.' })
+      return
+    }
+
+    const authenticationSuceed = await compare(password, rows[0].password_hash)
+
+    if (!authenticationSuceed) {
+      res.send({ message: '로그인에 실패했어요. 이메일 또는 비밀번호를 확인해주세요.' })
+      return
+    }
+
+    const jwt = await generateJWT({ userId: rows[0].id })
+    res.send({ jwt })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({ message: '서버 오류가 발생했어요' })
   }
-
-  const authenticationSuceed = await compare(password, rows[0].password_hash)
-
-  if (!authenticationSuceed) {
-    res.send({ message: '로그인에 실패했어요. 이메일 또는 비밀번호를 확인해주세요.' })
-    return
-  }
-
-  const jwt = await generateJWT({ userId: rows[0].id })
-  res.send({ jwt })
 })
 
 export default router
